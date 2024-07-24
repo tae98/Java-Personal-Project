310e11fd2de3e7f5352048a3a21508e34a34ca15
taehyunkim <98rlaxogus@gmail.com>
2024-07-24 17:54:30 +0900

    init commit lv1-3 lv1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,20 +7,21 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번쨰 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
 
-        System.out.print("두 번쨰 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String tmp =sc.next();
-        char operator = tmp.charAt(0);
-
-        int result =0;
 
         boolean flag = true;
         while(flag == true){
+
+            System.out.print("첫 번쨰 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("두 번쨰 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String tmp =sc.next();
+            int result =0;
+            char operator = tmp.charAt(0);
 
             switch(operator){
                 case '+':
@@ -42,7 +43,12 @@ public class App {
                     }
             }
             System.out.println("결과: " + result);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String endCommand = sc.next();
 
+            if(Objects.equals(endCommand, "exit")){
+                flag = false;
+            }
 
         }
 
