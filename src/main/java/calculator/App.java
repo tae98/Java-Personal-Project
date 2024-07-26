71edcf7040d9f822e708542f25f7610014ab2013
taehyunkim <98rlaxogus@gmail.com>
2024-07-26 15:54:22 +0900

    Lv2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,59 +4,79 @@ import java.util.Objects;
 import java.util.Queue;
 import java.util.Scanner;
 
+import static calculator.Calculator.getCircleQueue;
+
 public class App {
 
-    Calculator calculator = new Calculator();
+    static Calculator calculator = new Calculator();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double num1 = 0;
         double num2 = 0;
+        int calcDecide = 0;
+        double radius = 0;
+        double result = 0;
         char operator;
 
 
         //반복문으로 실행
         while(true){
-            System.out.print("첫 번쨰 숫자를 입력하세요: ");
-            num1 = sc.nextInt();
-            System.out.print("두 번쨰 숫자를 입력하세요: ");
-            num2 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            String tmp =sc.next();
-            operator = tmp.charAt(0);
-            //예외처리를위한 try문
-            try{
-                //Calculator 클래스의 메서들를활용
-                Calculator.calculate(operator, num1, num2);
+            System.out.println("원하시는 계산을 입력해주세요");
+            System.out.println("사칙연산을 원하시면 : 1");
+            System.out.println("원의 넓이를 계산하고 싶으면: 2");
+            calcDecide = sc.nextInt();
+            if(Objects.equals(calcDecide, 1)){
+                System.out.print("첫 번쨰 숫자를 입력하세요: ");
+                num1 = sc.nextInt();
+                System.out.print("두 번쨰 숫자를 입력하세요: ");
+                num2 = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                String tmp =sc.next();
+                operator = tmp.charAt(0);
+                //예외처리를위한 try문
+                try{
+                    //Calculator 클래스의 메서들를활용
+                    result = Calculator.calculate(operator, num1, num2);
+                    System.out.println(result);
 
-                //queue에저장된값제거
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                String removeTxt = sc.next();
+                    //queue에저장된값제거
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    String removeTxt = sc.next();
 
-                if(Objects.equals(removeTxt, "remove")){
-                    Calculator.getResultQueue().poll();
-                }
-                //queue에저장된값출력
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                String inquiryTxt = sc.next();
-                //inquiry가 입력됬을때 foreach문을 이용해 프린트
-                if(Objects.equals(inquiryTxt, "inquiry")){
-                    for(double num: Calculator.getResultQueue()){
-                        System.out.println(num);
+                    if(Objects.equals(removeTxt, "remove")){
+                        Calculator.getResultQueue().poll();
                     }
-                }
+                    //queue에저장된값출력
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    String inquiryTxt = sc.next();
 
-                //반복문 탈출
-                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
-                String endCommand = sc.next();
-                if(Objects.equals(endCommand, "exit")){
-                    break;
+                    //inquiry가 입력됬을때 inquiryQueue메서드 이용해 프린트
+                    if(Objects.equals(inquiryTxt, "inquiry")){
+                        calculator.inquiryQueue(calculator.getResultQueue());
+                    }//예외처리
+                }catch(CalculateException e){
+                    System.out.println(e.getMessage());
                 }
-            //예외처리
-            }catch(CalculateException e){
-                System.out.println(e.getMessage());
             }
+            else if(Objects.equals(calcDecide, 2)){
+                //반지름을 입력받아 계산
+                System.out.println("반지름의 값을 입력해주세요:");
+                radius = sc.nextDouble();
+                result = calculator.circleCalculate(radius);
+                System.out.println("결과: " + result);
 
+                //계산된값을 해당 큐에 저장
+                getCircleQueue().add(result);
+
+                //저장된 값들조회
+                calculator.inquiryQueue(getCircleQueue());
+            }
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String endCommand = sc.next();
+            if(Objects.equals(endCommand, "exit")){
+                break;
+            }
         }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,16 +4,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Calculator {
+    public static final double PI = 3.14159265358979323846;
 
     public Calculator(char operator, double num1, double num2) {
 
     }
     //캡슐화
     private static Queue<Double> resultQueue;
+    private static Queue<Double> circleQueue;
 
     //생성자&필드 초기화
     public Calculator(){
         this.resultQueue = new LinkedList<>();
+        this.circleQueue = new LinkedList<>();
     }
 
     public static double calculate(char operator, double num1, double num2) throws CalculateException {
@@ -43,13 +46,31 @@ public class Calculator {
         return result;
 
     }
-    //getter메서드
+    //getter메서드 for result
     public static Queue<Double> getResultQueue(){
         return resultQueue;
     }
-    //setter메서드
+    //setter메서드 for result
     public static void setResultQueue(Queue<Double> newResultQueue){
         resultQueue = newResultQueue;
+    }
+
+    //원 넓이 계산
+    public static double circleCalculate(double radius){
+        double area = 0;
+        area = PI * Math.pow(radius,2);
+        return area;
+    }
+    public void inquiryQueue(Queue<Double> q){
+        System.out.println(q);
+    }
+
+    public static Queue<Double> getCircleQueue(){
+        return circleQueue;
+    }
+
+    public static void setCircleQueue(Queue<Double> newCircleQueue){
+        circleQueue = newCircleQueue;
     }
 
 }
